46cce858d3bcb625192ac4ca93d365d713715244
YangDi <1356721352@qq.com>
2026-03-15 22:11:17 +0800

    玩家基本运动脚本编写

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float rotationSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+
+        direction = direction.normalized;// 单位化方向
+
+        transform.position += direction * Time.deltaTime * moveSpeed;
+
+        if (direction != Vector3.zero)
+        {
+            transform.forward = Vector3.Slerp(transform.forward, direction, Time.deltaTime * rotationSpeed);//Spherical Interpolation球面线性插值
+        }
+    }
+}
