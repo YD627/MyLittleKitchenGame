c5c35c076caa40ad9639947d6aa186769e83500c
YangDi <1356721352@qq.com>
2026-03-15 22:51:11 +0800

    为玩家添加待机和行走动画并用脚本控制

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float rotationSpeed = 10f;
+    private bool isWalking = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +20,9 @@ public class Player : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0, vertical);
 
+        if (direction != Vector3.zero) isWalking = true;
+        else isWalking = false;
+
         direction = direction.normalized;// 单位化方向
 
         transform.position += direction * Time.deltaTime * moveSpeed;
@@ -27,5 +31,9 @@ public class Player : MonoBehaviour
         {
             transform.forward = Vector3.Slerp(transform.forward, direction, Time.deltaTime * rotationSpeed);//Spherical Interpolation球面线性插值
         }
+    }
+    public bool IsWalking
+    {
+        get { return isWalking; }
     }
 }
